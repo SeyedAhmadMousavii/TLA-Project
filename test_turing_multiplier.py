54175d37bb3ca0cb7f975db3baa44d56ed225191
Seyed Ahmad Mousavi <seyedahmadmosavi29@gmail.com>
2026-06-06 15:17:41 +0330

    initializing test turing multiplier

--- test_turing_multiplier.py
+++ test_turing_multiplier.py
@@ -2,32 +2,49 @@
 from turing_machine import TuringMachine
 from test_turing_machine_example1 import print_states
 
-#create the Turing machine
 transitions = {
-        # TODO: Part II b) - Write your transition rules here as entries to a Python dictionary
-        # For example, the key will be a pair (state, character)
-        # The value will be the triple (next state, character to write, move head L or R)
-        # such as ('q0', '1'): ('q1', '0', 'R'), which says if current state is q0 and 1 encountered
-        # then transition to state q1, write a 0 and move head right.
+    ('q0', '1'): ('q1', 'x', 'R'),
+    ('q0', '0'): ('qh', '0', 'R'),
+    
+    ('q1', '1'): ('q1', '1', 'R'),
+    ('q1', '0'): ('q2', '0', 'R'),
+    
+    ('q2', '1'): ('q2', '1', 'R'),
+    ('q2', ''): ('q3', '', 'L'),
+    
+    ('q3', '1'): ('q4', 'y', 'L'),
+    ('q3', '0'): ('q0', '0', 'L'),
+    
+    ('q4', '1'): ('q4', '1', 'L'),
+    ('q4', '0'): ('q4', '0', 'L'),
+    ('q4', 'x'): ('q4', 'x', 'L'),
+    ('q4', ''): ('q5', '', 'R'),
+    
+    ('q5', 'x'): ('q2', 'x', 'R'),
+    ('q5', 'y'): ('q5', 'y', 'R'),
+    ('q5', '1'): ('q5', '1', 'R'),
+    ('q5', '0'): ('q5', '0', 'R'),
+    
+    ('q6', 'y'): ('q6', '1', 'R'),
+    
+    ('qh', 'x'): ('qh', '', 'R'),
+    ('qh', '1'): ('qh', '1', 'R'),
+    ('qh', 'y'): ('qh', '1', 'R'),
+    ('qh', ''): ('qa', '', 'R'),
 }
+
 if __name__ == "__main__":
     print_states(transitions)
     machine = TuringMachine(transitions)
 
     def run(input_):
         w = input_
-        print("Input:",w)
-        print("Accepted" if machine.accepts(w) else "Rejected")
-        machine.debug(w, step_limit=1000)
-
+        print("Input:", w)
+        result = machine.accepts(w, step_limit=500)
+        print("Accepted" if result else "Rejected")
+        machine.debug(w, step_limit=500)
         print()
 
-    # SHOULD ACCEPT
     run("110111")
-    # outputs 111111
-
-    # SHOULD ACCEPT
     run("11101111")
-    # outputs 111111111111
-
     run("01111")
