6e4927e61cd6af205d7dfdcf7ca37cc0d5416633
Seyed Ahmad Mousavi <seyedahmadmosavi29@gmail.com>
2026-06-06 15:16:54 +0330

    initializing test turing adder

--- test_turing_adder.py
+++ test_turing_adder.py
@@ -2,13 +2,14 @@
 from turing_machine import TuringMachine
 from test_turing_machine_example1 import print_states
 
-#create the Turing machine
 transitions = {
-        # TODO: Part II a) - Write your transition rules here as entries to a Python dictionary
-        # For example, the key will be a pair (state, character)
-        # The value will be the triple (next state, character to write, move head L or R)
-        # such as ('q0', '1'): ('q1', '0', 'R'), which says if current state is q0 and 1 encountered
-        # then transition to state q1, write a 0 and move head right.
+    ('q0', '1'): ('q0', '1', 'R'),
+    ('q0', '0'): ('q1', '1', 'R'),
+    
+    ('q1', '1'): ('q1', '1', 'R'),
+    ('q1', ''): ('q2', '', 'L'),
+    
+    ('q2', '1'): ('qa', '', 'R'),
 }
 
 if __name__ == "__main__":
@@ -17,17 +18,12 @@ if __name__ == "__main__":
 
     def run(input_):
         w = input_
-        print("Input:",w)
-        print("Accepted" if machine.accepts(w) else "Rejected")
-        machine.debug(w)
+        print("Input:", w)
+        result = machine.accepts(w, step_limit=200)
+        print("Accepted" if result else "Rejected")
+        machine.debug(w, step_limit=200)
         print()
 
-    # SHOULD ACCEPT
     run("110111")
-    # outputs 11111
-
-    # SHOULD ACCEPT
     run("11101111")
-    #     # outputs 1111111
     run("0111")
-    # outputs 111
